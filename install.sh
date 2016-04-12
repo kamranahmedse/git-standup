4665a3173b448e532fdfc8985916208c9649bc36
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-04-12 20:10:58 +0400

    Update installer

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/kamranahmedse/git-standup.git
-cd git-standup
-make install
-cd ..
-rm -rf git-standup
---
+++ installer.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## Clone the repo
+git clone https://github.com/kamranahmedse/git-standup.git || { echo >&2 "Clone failed with $?"; exit 1; }
+cd git-standup
+make install || { echo >&2 "Clone failed with $?"; exit 1; }
+cd ..
+rm -rf git-standup
