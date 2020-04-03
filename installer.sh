10c9c59b2ca8745b4a4610797d947956bcb76cb2
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-04-03 13:21:59 +0400

    Run formatter

--- installer.sh
+++ installer.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 ## Clone the repo
-git clone https://github.com/kamranahmedse/git-standup.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
+git clone https://github.com/kamranahmedse/git-standup.git --depth=1 || {
+  echo >&2 "Clone failed with $?"
+  exit 1
+}
 cd git-standup
-make install || { echo >&2 "Clone failed with $?"; exit 1; }
+make install || {
+  echo >&2 "Clone failed with $?"
+  exit 1
+}
 cd ..
 rm -rf git-standup
